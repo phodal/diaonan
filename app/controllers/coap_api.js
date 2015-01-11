4206c326300b814b9d7826543c202891d9d222b2
Fengda HUANG <h@phodal.com>
2015-01-11 09:57:06 +0800

    Add coap get

--- app/controllers/coap_api.js
+++ app/controllers/coap_api.js
@@ -2,10 +2,31 @@ module.exports = function (app) {
 	var Data;
 	Data = app.models.Data;
 	return function (req, res) {
-		console.log(req,res);
 		var handlerGet = function () {
-			res.code = '2.05';
-			res.end({});
+			console.log(/^\/topics\/(.+)$/.exec(req.url));
+			if (/^\/topics\/(.+)$/.exec(req.url) === null){
+				res.code = '4.04';
+				res.end({error: 4.04});
+				return;
+			}
+			var topic = /^\/topics\/(.+)$/.exec(req.url)[1];
+			return Data.find(topic, function (err, data) {
+				var e, type;
+				if (err !== null) {
+					res.code = '4.04';
+					res.end({error: 4.04});
+				} else {
+					try {
+						res.code = '2.05';
+						res.end(data.value);
+					} catch (_error) {
+						e = _error;
+						console.log(e);
+						res.code = '2.06';
+						res.end({error: 4.04, message: e});
+					}
+				}
+			});
 		};
 
 		var handPost = function () {
@@ -22,10 +43,6 @@ module.exports = function (app) {
 				break;
 		}
 
-		return req.on('request', function (packet) {
-			return req.unsuback({
-				messageId: packet.messageId
-			});
-		});
+		return ;
 	};
 };
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,17 +1,17 @@
-module.exports = function(app) {
-  app.get('/designiot', function(req, res) {
-    var _base;
-    (_base = req.session).topics || (_base.topics = []);
-    return res.render('designiot.hbs', {
-      topics: req.session.topics
-    });
-  });
+module.exports = function (app) {
+	app.get('/dream', function (req, res) {
+		var _base;
+		(_base = req.session).topics || (_base.topics = []);
+		return res.render('dream.hbs', {
+			topics: req.session.topics
+		});
+	});
 
-  return app.get('/', function(req, res) {
-    var _base;
-    (_base = req.session).topics || (_base.topics = []);
-    return res.render('home.hbs', {
-      topics: req.session.topics
-    });
-  });
+	return app.get('/', function (req, res) {
+		var _base;
+		(_base = req.session).topics || (_base.topics = []);
+		return res.render('home.hbs', {
+			topics: req.session.topics
+		});
+	});
 };
