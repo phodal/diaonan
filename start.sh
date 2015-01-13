4a1d57b8206e2bffc17283267825caca066a5c26
Phodal <h@phodal.com>
2015-01-13 03:26:38 +0000

    Add node env

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 export NVM_DIR="/root/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm use 0.10.35
+NODE_ENV=development
 forever stopall &
 forever start -l /home/www/diaonan/log/forever.log -o /home/www/diaonan/log/out.log -e /home/www/diaonan/log/err.log -a qest.js
