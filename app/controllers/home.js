40c69e49aafb60ca2308044700697b4caf871311
Fengda HUANG <h@phodal.com>
2015-02-04 17:40:02 +0800

    Remove unuse library

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,12 +1,4 @@
 module.exports = function (app) {
-	app.get('/dream', function (req, res) {
-		var _base;
-		(_base = req.session).topics || (_base.topics = []);
-		return res.render('dream.hbs', {
-			topics: req.session.topics
-		});
-	});
-
 	return app.get('/', function (req, res) {
 		var _base;
 		(_base = req.session).topics || (_base.topics = []);
